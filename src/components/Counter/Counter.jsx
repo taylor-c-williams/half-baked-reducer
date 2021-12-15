b9a1d889b32c2a918a059d17a2e7c6692829f31d
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-12-15 14:50:03 -0800

    mvp

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,47 +1,69 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useReducer } from 'react'
 
 const pinkRGB = `rgb(236, 72, 153)`
 
+const initialState = { count: 0 }
+
+function counterReducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }
+    case 'reset':
+      return { count: 0 }
+    default: {
+      throw new Error(`No matching 'type' for ${action.type}`)
+    }
+  }
+}
+
 export default function Counter() {
-  const [count, setCount] = useState(0)
   const [currentColor, setCurrentColor] = useState(pinkRGB)
+  const [count, dispatch] = useReducer(counterReducer, initialState)
+
+  const handleIncrement = () => {
+    dispatch({
+      type: 'increment',
+    })
+  }
+
+  const handleDecrement = () => {
+    dispatch({
+      type: 'decrement',
+    })
+  }
+
+  const handleReset = () => {
+    dispatch({
+      type: 'reset',
+    })
+  }
 
   useEffect(() => {
-    if (count === 0) {
+    if (count.count === 0) {
       setCurrentColor(pinkRGB)
     }
 
-    if (count > 0) {
+    if (count.count > 0) {
       setCurrentColor(`rgb(52, 211, 153)`)
     }
 
-    if (count < 0) {
+    if (count.count < 0) {
       setCurrentColor(`rgb(239, 68, 68)`)
     }
   }, [count])
 
-  const increment = () => {
-    setCount((prevState) => prevState + 1)
-  }
-
-  const decrement = () => {
-    setCount((prevState) => prevState - 1)
-  }
-
-  const reset = () => {
-    setCount(0)
-  }
-
   return (
     <main className="bg-black bg-opacity-90 min-h-screen flex flex-col items-center justify-center text-4xl text-pink-500">
       <h1 className="mb-5" style={{ color: currentColor }}>
-        {count}
+        {count.count}
       </h1>
       <div className="flex w-1/2 justify-around">
         <button
           className="text-green-400 border-2 border-green-400 p-3"
           type="button"
-          onClick={increment}
+          onClick={handleIncrement}
           aria-label="increment"
         >
           Increment
@@ -49,7 +71,7 @@ export default function Counter() {
         <button
           className="text-red-500 border-2 border-red-500 p-2"
           type="button"
-          onClick={decrement}
+          onClick={handleDecrement}
           aria-label="decrement"
         >
           Decrement
@@ -58,7 +80,7 @@ export default function Counter() {
           className="text-pink-500 border-2 border-pink-500 p-2"
           type="button"
           aria-label="reset"
-          onClick={reset}
+          onClick={handleReset}
         >
           Reset
         </button>
